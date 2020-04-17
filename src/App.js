8cc51b3bbcc5fdda71b624ea25d897226e8f77d6
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-04-17 22:45:50 +0300

    create component Post

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Post from './Components/Post';
+import posts from './posts.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          posts.map((post, key) => {
+            return (
+              <Post
+                key={key}
+                title={post.title}
+                description={post.description}
+                image={post.image}
+              />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
