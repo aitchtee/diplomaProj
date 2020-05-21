c83579301eb9f1205be9ca431660537ddb42c505
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-05-21 20:27:09 +0300

    move configs files

---
+++ server/src/config/configs.js
@@ -0,0 +1,30 @@
+//* объект, в котором хранятся параметры для поиска 
+const elems = {
+  krasnodar: {
+    tag: '.material__tag',
+    title: '.material__name',
+    image: '.figure a img',
+    text: '.material__content_detail_text',
+  },
+  sochi: {
+    tag:'ul li',
+    title: '.h4-gorod',
+    image: '.detail_picture img',
+    text: '.detail_text',
+  },
+  novorossiysk: {
+    // tag: '.news-detail__category',
+    // title: '.news-detail__header h1',
+    // image: '.news-detail-content img',
+    // text: '.decor p',
+    tag: '.td-category',
+    title: '.entry-title',
+    image: '.td-post-content img',
+    text: '.td-post-content',
+  },
+};
+
+// делаем возможным передать объект в другие файлы
+module.exports = {
+  elems: elems, // передаем объект elems с конфигурациями для поиска
+};
---
+++ server/src/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb+srv://mrfleen_23:professor23@cluster0-ylzwd.mongodb.net/test'
+  };
