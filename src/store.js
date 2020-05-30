bcb7741ce41341421f9074697632b0266a3e74bb
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-05-31 00:17:41 +0300

    remove unnecessary files

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger'; // нужен чтобы отслеживать изменения store в консоли браузера
-
-import rootReducers from './reducers';
-
-const store = createStore(rootReducers, applyMiddleware(logger));
-
-export default store;
