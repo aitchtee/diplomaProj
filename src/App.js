68e4a5f5007a22e162eecfe072e32af9d624279d
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-06-06 14:18:19 +0300

    done project

--- src/App.js
+++ src/App.js
@@ -1,103 +1,33 @@
 import React, { Component } from 'react';
-import Post from './components/Post';
-import axios from 'axios';
-import { connect } from 'react-redux'; // используется, когда нужно при экспорте требуется подключить что-то
-import {
-  Container, Header, Button, Item,
-  Dimmer, Loader, Image, Segment
-} from 'semantic-ui-react'
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import City from './Pages/City';
+import Tag from './Pages/Tag';
+import PostDetails from './Pages/PostDetails';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-
-  // constructor(props) {
-  //   super(props);
-  // };
-
-  fetchPosts() {
-    const { setPosts } = this.props;
-    setPosts([]);
-    axios
-      .get('https://5ea4b7ee2d86f00016b453f1.mockapi.io/posts')
-      .then(({ data }) => {
-        setPosts(data);
-      });
-  };
-
-  componentWillMount() { // когда компонент будет монтироваться
-    this.fetchPosts();
-  };
-
-  regionText(text) {
-    switch (text) {
-      case 'Krasnodar':
-        return 'Краснодар';
-      case 'Sochi':
-        return 'Сочи';
-      case 'Novorossiysk':
-        return 'Новороссийск';
-      default:
-    }
-  }
-
-
-
-  // ToDo: реализовать переход между регионами. Открытие определенной новости. Подключить базу. Сделать header
-
   render() {
-    const { posts } = this.props;
-    const { items } = posts;
     return (
-      <Container>
-        <Header as='h2'>Regions: {this.regionText(this.props.regions.region)}</Header>
-        <Button.Group basic>
-          <Button onClick={() => this.props.changeRegion('Krasnodar')}>Krasnodar</Button>
-          <Button onClick={() => this.props.changeRegion('Sochi')}>Sochi</Button>
-          <Button onClick={() => this.props.changeRegion('Novorossiysk')}>Novorossiysk</Button>
-        </Button.Group>
-        <Item.Group divided>
-          {
-            !items.length ? (
-              <Segment Loader>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <Dimmer active inverted>
-                  <Loader inverted size='large'>Loading</Loader>
-                </Dimmer>
-                <Image src='/images/wireframe/short-paragraph.png' />
-              </Segment>
-            ) : (
-                items.map((item, key) => (
-                  <Post
-                    key={key}
-                    {...item} // все свойства из item будут присвоены компоненту
-                  />
-                ))
-              )
-          }
-        </Item.Group>
-      </Container >
-    );
+      <>
+        <Router>
+          <div className="App">
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/:city" component={City} />
+              <Route exact path="/:city/:tag" component={Tag} />
+              <Route exact path="/:city/:tag/:post_id" component={PostDetails} />
+            </Switch>
+          </div>
+        </Router>
+      </>
+    )
   }
 }
 
-const state = props => {
-  // console.log(props);
-  return props;
-};
+export default App;
 
-const actions = (dispatch) => ({
-  setPosts: (data) => dispatch({
-    type: 'SET_POSTS',
-    payload: data,
-  }),
-  changeRegion: (name) => dispatch({
-    type: 'CHANGE_REGION',
-    payload: name,
-  })
-});
-
-export default connect(state, actions)(App);
